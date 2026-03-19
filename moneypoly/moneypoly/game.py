22c42692bbfa3465caefd70cfcbd58732b24454f
Mahek <mahek22d@gmail.com>
2026-03-19 22:50:44 +0530

    Iteration 15: Disable too-many-instance-attributes warning for Game class

--- moneypoly/moneypoly/game.py
+++ moneypoly/moneypoly/game.py
@@ -17,7 +17,7 @@ from moneypoly.dice import Dice
 from moneypoly.cards import CardDeck, CHANCE_CARDS, COMMUNITY_CHEST_CARDS
 from moneypoly import ui
 
-
+# pylint: disable=too-many-instance-attributes
 class Game:
     """Manages the full state and flow of a MoneyPoly game session."""
 
@@ -467,4 +467,3 @@ class Game:
             f"  Cash to receive from {partner.name}: $", default=0
         )
         self.trade(player, partner, chosen_prop, cash)
-        
