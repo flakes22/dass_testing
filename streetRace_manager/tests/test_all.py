b54191dd4c4f1f30c46aac1ad4e84fc022b3aa18
Mahek <mahek22d@gmail.com>
2026-03-20 16:38:12 +0530

    Unit test for Vehicle Upgrade Module

--- streetRace_manager/tests/test_all.py
+++ streetRace_manager/tests/test_all.py
@@ -4,6 +4,7 @@ from test_inventory import run_inventory_tests
 from test_race_management import run_race_management_tests
 from test_results import run_results_tests
 from test_mission_planning import run_mission_tests
+from test_vehicle_upgrade import run_vehicle_upgrade_tests
 
 if __name__ == "__main__":
     run_registration_tests()
@@ -12,4 +13,5 @@ if __name__ == "__main__":
     run_race_management_tests()
     run_results_tests()
     run_mission_tests()
+    run_vehicle_upgrade_tests()
 
