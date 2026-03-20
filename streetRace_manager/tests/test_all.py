7c27616edf7fca32271d2ab97accb9fcf2afe9f1
Mahek <mahek22d@gmail.com>
2026-03-20 16:01:12 +0530

    Unit test for Crew Mangement Module

--- streetRace_manager/tests/test_all.py
+++ streetRace_manager/tests/test_all.py
@@ -1,4 +1,6 @@
 from test_registration import run_registration_tests
+from test_crew_management import run_crew_management_tests
 
 if __name__ == "__main__":
     run_registration_tests()
+    run_crew_management_tests()
