7e383fef8c03ec14fbe79607f259fb31c12e46c2
Mahek <mahek22d@gmail.com>
2026-03-20 16:05:08 +0530

    Unit test for Inventory Module

--- streetRace_manager/tests/test_all.py
+++ streetRace_manager/tests/test_all.py
@@ -1,6 +1,9 @@
 from test_registration import run_registration_tests
 from test_crew_management import run_crew_management_tests
+from test_inventory import run_inventory_tests
 
 if __name__ == "__main__":
     run_registration_tests()
     run_crew_management_tests()
+    run_inventory_tests()
+    
